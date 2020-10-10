c73dee9c5a6e85680d95ba3e270143ebee2d6cc0
Brahian <minimon1814@hotmail.com>
2020-10-10 07:17:19 -0400

    Changes on the mobile scripts
    Changes on the mobile scripts

---
+++ Test Suites/Mobile/Check Login Functionalities.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Check Login Functionalities</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>45159dcf-70db-46e6-923d-52d096ee4930</testSuiteGuid>
+   <testCaseLink>
+      <guid>21516d97-59f4-433f-972f-1cfe50021ead</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check Login Functionalities Mobile/Check System behavior with a valid email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2d7fc5f8-ca56-4f39-a838-1ff7662bbc1b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check Login Functionalities Mobile/Check System Behavior with a valid phone number</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f0f66991-8c2f-4d17-b05c-082312d53081</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check Login Functionalities Mobile/Check System behavior with an invalid email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8d756c8c-e701-4281-8182-489f48dc7063</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check Login Functionalities Mobile/Check System Behavior with an invalid phone number</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
